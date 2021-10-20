a399ee53f64b76605dbc03639bb1118a6b6ea485
Bryan C Guner <bryan.guner@gmail.com>
2021-10-20 03:53:07 +0000

    update

--- adorable-saturn-f97de/studio/schemas/testimonials_section.js
+++ adorable-saturn-f97de/studio/schemas/testimonials_section.js
@@ -1,70 +1,67 @@
 export default {
-    "type": "object",
-    "name": "testimonials_section",
-    "title": "Testimonials Section",
-    "fields": [
+  type: "object",
+  name: "testimonials_section",
+  title: "Testimonials Section",
+  fields: [
+    {
+      type: "string",
+      name: "section_id",
+      title: "ID",
+      description:
+        "A unique identifier of the section, must not contain whitespace",
+      validation: null,
+    },
+    {
+      type: "string",
+      name: "title",
+      title: "Title",
+      description: "The title of the section",
+      validation: null,
+    },
+    {
+      type: "string",
+      name: "subtitle",
+      title: "Subtitle",
+      description: "The subtitle of the section",
+      validation: null,
+    },
+    {
+      type: "array",
+      name: "testimonials",
+      title: "Testimonials",
+      validation: null,
+      of: [
         {
-            "type": "string",
-            "name": "section_id",
-            "title": "ID",
-            "description": "A unique identifier of the section, must not contain whitespace",
-            "validation": null
+          type: "testimonial",
         },
-        {
-            "type": "string",
-            "name": "title",
-            "title": "Title",
-            "description": "The title of the section",
-            "validation": null
-        },
-        {
-            "type": "string",
-            "name": "subtitle",
-            "title": "Subtitle",
-            "description": "The subtitle of the section",
-            "validation": null
-        },
-        {
-            "type": "array",
-            "name": "testimonials",
-            "title": "Testimonials",
-            "validation": null,
-            "of": [
-                {
-                    "type": "testimonial"
-                }
-            ]
-        },
-        {
-            "type": "string",
-            "name": "type",
-            "title": "Object Type",
-            "description": "The type of the object",
-            "hidden": false,
-            "validation": Rule => Rule.required(),
-            "options": {
-                "list": [
-                    "testimonials_section"
-                ]
-            }
-        },
-        {
-            "type": "string",
-            "name": "stackbit_model_type",
-            "title": "Stackbit Model Type",
-            "description": "Stackbit model type",
-            "hidden": false,
-            "validation": Rule => Rule.required(),
-            "options": {
-                "list": [
-                    "object"
-                ]
-            }
-        }
-    ],
-    "preview": {
-        "select": {
-            "title": "title"
-        }
-    }
-}
+      ],
+    },
+    {
+      type: "string",
+      name: "type",
+      title: "Object Type",
+      description: "The type of the object",
+      hidden: false,
+      validation: (Rule) => Rule.required(),
+      options: {
+        list: ["testimonials_section"],
+      },
+    },
+    {
+      type: "string",
+      name: "stackbit_model_type",
+      title: "Stackbit Model Type",
+      description: "Stackbit model type",
+      hidden: false,
+      validation: (Rule) => Rule.required(),
+      options: {
+        list: ["object"],
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+    },
+  },
+};
