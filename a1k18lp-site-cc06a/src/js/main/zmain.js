87f092bfdd27212fc4bd282d3f286896eecc6f67
Bryan C Guner <bryan.guner@gmail.com>
2021-10-20 03:50:40 +0000

    update

--- a1k18lp-site-cc06a/src/js/main/zmain.js
+++ a1k18lp-site-cc06a/src/js/main/zmain.js
@@ -1,12 +1,12 @@
-(function( $, window, undefined ) {
+(function ($, window, undefined) {
   // Menu
-  $("#menu").click(function() {
+  $("#menu").click(function () {
     $("body").addClass("push-menu-to-right");
     $("#sidebar").addClass("open");
     $(".overlay").addClass("show");
   });
 
-  $("#mask").click(function() {
+  $("#mask").click(function () {
     $("body").removeClass("push-menu-to-right");
     $("#sidebar").removeClass("open");
     $(".overlay").removeClass("show");
@@ -17,28 +17,27 @@
     var top = $(this).scrollTop();
     if (top > 0) {
       $("body").addClass("light");
-    }
-    else {
+    } else {
       $("body").removeClass("light");
     }
   });
 
   // Modals
 
-  var $closeBtn = $('.modal .close');
-  $closeBtn.on('click', function() {
-      $closeBtn.parent().parent().addClass('closed');
+  var $closeBtn = $(".modal .close");
+  $closeBtn.on("click", function () {
+    $closeBtn.parent().parent().addClass("closed");
   });
 
-  var $exitModal = $('.modal.exit');
+  var $exitModal = $(".modal.exit");
   if ($exitModal.length) {
     ouibounce($exitModal[0], {
       aggressive: true,
-      callback: function() {
-        $exitModal.find('.close').on('click', function() {
+      callback: function () {
+        $exitModal.find(".close").on("click", function () {
           $exitModal.hide();
         });
-      }
+      },
     });
   }
 
@@ -47,45 +46,44 @@
     close: $(".icon-remove-sign"),
     searchform: $(".search-form"),
     canvas: $("body"),
-    dothis: $('.dosearch')
+    dothis: $(".dosearch"),
   };
 
-  bs.dothis.on('click', function() {
-    $('.search-wrapper').toggleClass('active');
-    bs.searchform.toggleClass('active');
-    bs.searchform.find('input').focus();
-    bs.canvas.toggleClass('search-overlay');
-    $('.search-field').simpleJekyllSearch();
+  bs.dothis.on("click", function () {
+    $(".search-wrapper").toggleClass("active");
+    bs.searchform.toggleClass("active");
+    bs.searchform.find("input").focus();
+    bs.canvas.toggleClass("search-overlay");
+    $(".search-field").simpleJekyllSearch();
   });
 
   function close_search() {
-    $('.search-wrapper').toggleClass('active');
-    bs.searchform.toggleClass('active');
-    bs.canvas.removeClass('search-overlay');
+    $(".search-wrapper").toggleClass("active");
+    bs.searchform.toggleClass("active");
+    bs.canvas.removeClass("search-overlay");
   }
 
-  bs.close.on('click', close_search);
+  bs.close.on("click", close_search);
 
   // Closing menu with ESC
-  document.addEventListener('keyup', function(e){
-      if(e.keyCode == 27 && $('.search-overlay').length) {
-          close_search();
-      }
+  document.addEventListener("keyup", function (e) {
+    if (e.keyCode == 27 && $(".search-overlay").length) {
+      close_search();
+    }
   });
-  
-  if (document.getElementsByClassName('home').length >=1 ) {
-      new AnimOnScroll( document.getElementById( 'grid' ), {
-        minDuration : 0.4,
-        maxDuration : 0.7,
-        viewportFactor : 0.2
-      });
+
+  if (document.getElementsByClassName("home").length >= 1) {
+    new AnimOnScroll(document.getElementById("grid"), {
+      minDuration: 0.4,
+      maxDuration: 0.7,
+      viewportFactor: 0.2,
+    });
   }
 
   // Init smooth scroll
   smoothScroll.init({
-      selectorHeader: '.bar-header', // Selector for fixed headers (must be a valid CSS selector)
-      speed: 500, // Integer. How fast to complete the scroll in milliseconds
-      updateURL: false // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    selectorHeader: ".bar-header", // Selector for fixed headers (must be a valid CSS selector)
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
   });
-
-})( Zepto, window );
+})(Zepto, window);
