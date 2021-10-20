a6e9607d420405572f76e57b153fb92aa495813b
Bryan C Guner <bryan.guner@gmail.com>
2021-10-20 03:53:24 +0000

    update

--- adorable-sycamore-2086e/assets/js/main.js
+++ adorable-sycamore-2086e/assets/js/main.js
@@ -3,30 +3,35 @@
  */
 
 // Responsive video embeds
-var videoEmbeds = [
-  'iframe[src*="youtube.com"]',
-  'iframe[src*="vimeo.com"]'
-];
-reframe(videoEmbeds.join(','));
+var videoEmbeds = ['iframe[src*="youtube.com"]', 'iframe[src*="vimeo.com"]'];
+reframe(videoEmbeds.join(","));
 
 // Mobile menu
-var menuToggle = document.getElementById('menu-toggle');
+var menuToggle = document.getElementById("menu-toggle");
 if (menuToggle) {
-  menuToggle.addEventListener('click', function(e){
-    document.body.classList.toggle('menu--opened');
-    e.preventDefault();
-  },false);
-  document.body.classList.remove('menu--opened');
+  menuToggle.addEventListener(
+    "click",
+    function (e) {
+      document.body.classList.toggle("menu--opened");
+      e.preventDefault();
+    },
+    false
+  );
+  document.body.classList.remove("menu--opened");
 
-  window.addEventListener('resize', function () {
-    if (menuToggle.offsetParent === null) {
-      document.body.classList.remove('menu--opened');
-    }
-  }, true);
+  window.addEventListener(
+    "resize",
+    function () {
+      if (menuToggle.offsetParent === null) {
+        document.body.classList.remove("menu--opened");
+      }
+    },
+    true
+  );
 }
 
 // Back to top
-document.querySelector('#to-top').addEventListener('click', function (e) {
+document.querySelector("#to-top").addEventListener("click", function (e) {
   e.preventDefault();
-  document.querySelector('#page').scrollIntoView({ behavior: 'smooth' });
+  document.querySelector("#page").scrollIntoView({ behavior: "smooth" });
 });
