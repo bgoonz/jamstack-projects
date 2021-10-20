8aeef639e371b3f8f74c089c4eeaa0febde5c84a
Bryan C Guner <bryan.guner@gmail.com>
2021-10-20 03:50:44 +0000

    update

--- aawdev-6bdff/src/pages/_app.tsx
+++ aawdev-6bdff/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from 'next/app';
+import { AppProps } from "next/app";
 
-import '../styles/main.css';
+import "../styles/main.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
