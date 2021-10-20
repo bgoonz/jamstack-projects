ff47ab95f985d045fdb520764965675e7acca8ad
Bryan C Guner <bryan.guner@gmail.com>
2021-10-20 03:52:56 +0000

    update

--- adorable-pluto-2f72b/src/components/index.js
+++ adorable-pluto-2f72b/src/components/index.js
@@ -1,33 +1,33 @@
-import CtaButtons from './CtaButtons';
-import Footer from './Footer';
-import FormField from './FormField';
-import Header from './Header';
-import Layout from './Layout';
-import SectionContent from './SectionContent';
-import SectionForm from './SectionForm';
-import SectionHero from './SectionHero';
-import SectionPosts from './SectionPosts';
+import CtaButtons from "./CtaButtons";
+import Footer from "./Footer";
+import FormField from "./FormField";
+import Header from "./Header";
+import Layout from "./Layout";
+import SectionContent from "./SectionContent";
+import SectionForm from "./SectionForm";
+import SectionHero from "./SectionHero";
+import SectionPosts from "./SectionPosts";
 
 export {
-    CtaButtons,
-    Footer,
-    FormField,
-    Header,
-    Layout,
-    SectionContent,
-    SectionForm,
-    SectionHero,
-    SectionPosts
+  CtaButtons,
+  Footer,
+  FormField,
+  Header,
+  Layout,
+  SectionContent,
+  SectionForm,
+  SectionHero,
+  SectionPosts,
 };
 
 export default {
-    CtaButtons,
-    Footer,
-    FormField,
-    Header,
-    Layout,
-    SectionContent,
-    SectionForm,
-    SectionHero,
-    SectionPosts
+  CtaButtons,
+  Footer,
+  FormField,
+  Header,
+  Layout,
+  SectionContent,
+  SectionForm,
+  SectionHero,
+  SectionPosts,
 };
