2ca81404cb8d036ac285582644b606180e68099c
Bryan C Guner <bryan.guner@gmail.com>
2021-10-20 03:53:02 +0000

    update

--- adorable-sage-05157/src/layouts/page.js
+++ adorable-sage-05157/src/layouts/page.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import _ from 'lodash';
+import React from "react";
+import _ from "lodash";
 
-import {Layout} from '../components/index';
-import {htmlToReact, withPrefix, markdownify} from '../utils';
+import { Layout } from "../components/index";
+import { htmlToReact, withPrefix, markdownify } from "../utils";
 
 export default class Page extends React.Component {
-    render() {
-        return (
-            <Layout {...this.props}>
-            <div className="outer">
-              <div className="inner-medium">
-                <article className="post post-full">
-                  <header className="post-header">
-                    <h1 className="post-title">{_.get(this.props, 'page.title', null)}</h1>
-                    {_.get(this.props, 'page.subtitle', null) && (
-                    <div className="post-subtitle">
-                      {htmlToReact(_.get(this.props, 'page.subtitle', null))}
-                    </div>
-                    )}
-                  </header>
-                  {_.get(this.props, 'page.image', null) && (
-                  <div className="post-image">
-                    <img src={withPrefix(_.get(this.props, 'page.image', null))} alt={_.get(this.props, 'page.image_alt', null)} />
+  render() {
+    return (
+      <Layout {...this.props}>
+        <div className="outer">
+          <div className="inner-medium">
+            <article className="post post-full">
+              <header className="post-header">
+                <h1 className="post-title">
+                  {_.get(this.props, "page.title", null)}
+                </h1>
+                {_.get(this.props, "page.subtitle", null) && (
+                  <div className="post-subtitle">
+                    {htmlToReact(_.get(this.props, "page.subtitle", null))}
                   </div>
-                  )}
-                  <div className="post-content">
-                    {markdownify(_.get(this.props, 'page.content', null))}
-                  </div>
-                </article>
+                )}
+              </header>
+              {_.get(this.props, "page.image", null) && (
+                <div className="post-image">
+                  <img
+                    src={withPrefix(_.get(this.props, "page.image", null))}
+                    alt={_.get(this.props, "page.image_alt", null)}
+                  />
+                </div>
+              )}
+              <div className="post-content">
+                {markdownify(_.get(this.props, "page.content", null))}
               </div>
-            </div>
-            </Layout>
-        );
-    }
+            </article>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
